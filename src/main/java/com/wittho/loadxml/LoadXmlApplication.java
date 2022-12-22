b5a8f518fd8738c1646a9201d1a48da9aa3f4eec
marcusWittho <witthoeft.mcs@hotmail.com>
2022-12-22 00:03:17 -0300

    First commit

---
+++ src/main/java/com/wittho/loadxml/LoadXmlApplication.java
@@ -0,0 +1,13 @@
+package com.wittho.loadxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadXmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wittho/loadxml/LoadXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wittho.loadxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
